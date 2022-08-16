f22c6a3bed2e1d6d0fb5d9e61db09aaff72becf7
ghazal Alikhani <ghazal.alikhani123@Gmail.com>
2022-08-16 11:18:50 +0430

    change edit text number of show in each page

--- app/src/main/java/com/example/myapplication/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/MainFragment.kt
@@ -2,10 +2,15 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_ENTER
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.viewModels
 import com.example.myapplication.databinding.FragmentMainBinding
 import com.google.gson.Gson
@@ -17,6 +22,7 @@ class MainFragment : Fragment() {
     lateinit var binding: FragmentMainBinding
     val viewModel: CustomerViewModel by viewModels()
     var counterOfList = 0
+    var numberOfClassification = 8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,62 +42,69 @@ class MainFragment : Fragment() {
         val arrayCustomerType = object : TypeToken<Array<Customers>>() {}.type
         var arrayOfCustomer: Array<Customers> =
             gson.fromJson(viewModel.getJson(), arrayCustomerType)
-        var sliceCustomer = ArrayList<Customers>()
-        sliceCustomer.addAll(setData(arrayOfCustomer))
-        val customerAdaptor = CustomAdapter(sliceCustomer)
-        binding.customerRecyclerview.adapter = customerAdaptor
-        var currentPageNumber = 0
         var allPageNumber = 0
-        if(arrayOfCustomer.size % 10 == 0){
-            allPageNumber = arrayOfCustomer.size / 10
-        }else{
-            println("sliceCustomer.size / 10 ${sliceCustomer.size / 10}")
-            allPageNumber = (arrayOfCustomer.size / 10) + 1
-        }
-        viewModel.number.observe(viewLifecycleOwner) {
-            binding.textViewCurrentPage.text = it.toString()
-        }
-       binding.textViewAllPage.text = allPageNumber.toString()
-
-
-        fun clearListAddData() {
-            sliceCustomer.clear()
+        fun setInformation() {
+            var sliceCustomer = ArrayList<Customers>()
             sliceCustomer.addAll(setData(arrayOfCustomer))
-            customerAdaptor.notifyDataSetChanged()
-        }
-        if (counterOfList <= 0) {
-            binding.buttonPerv.isEnabled = false
-        }
-        binding.buttonNext.setOnClickListener {
-            counterOfList += 10
-            binding.buttonPerv.isEnabled = true
-            clearListAddData()
-            if (counterOfList in (arrayOfCustomer.size-9)..arrayOfCustomer.size) {
-                binding.buttonNext.isEnabled = false
+            val customerAdaptor = CustomAdapter(sliceCustomer)
+            binding.customerRecyclerview.adapter = customerAdaptor
+            if (arrayOfCustomer.size % numberOfClassification == 0) {
+                allPageNumber = arrayOfCustomer.size / numberOfClassification
+            } else {
+                allPageNumber = (arrayOfCustomer.size / numberOfClassification) + 1
             }
-            viewModel.riseNumber()
-        }
-        binding.buttonPerv.setOnClickListener {
-            counterOfList -= 10
-            viewModel.decNumber()
-            binding.buttonNext.isEnabled = true
-            clearListAddData()
+            viewModel.number.observe(viewLifecycleOwner) {
+                binding.textViewCurrentPage.text = it.toString()
+            }
+            binding.textViewAllPage.text = allPageNumber.toString()
+            fun clearListAddData() {
+                sliceCustomer.clear()
+                sliceCustomer.addAll(setData(arrayOfCustomer))
+                customerAdaptor.notifyDataSetChanged()
+            }
             if (counterOfList <= 0) {
                 binding.buttonPerv.isEnabled = false
             }
+            binding.buttonNext.setOnClickListener {
+                counterOfList += numberOfClassification
+                binding.buttonPerv.isEnabled = true
+                clearListAddData()
+                if (counterOfList in (arrayOfCustomer.size - (numberOfClassification - 1))..arrayOfCustomer.size) {
+                    binding.buttonNext.isEnabled = false
+                }
+                viewModel.riseNumber()
+            }
+            binding.buttonPerv.setOnClickListener {
+                counterOfList -= numberOfClassification
+                viewModel.decNumber()
+                binding.buttonNext.isEnabled = true
+                clearListAddData()
+                if (counterOfList <= 0) {
+                    binding.buttonPerv.isEnabled = false
+                }
+            }
         }
+        setInformation()
 
-
+        binding.editTextAlPage.setOnEditorActionListener { v, actionId, event ->
+            if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                numberOfClassification = binding.editTextAlPage.text.toString().toInt()
+                setInformation()
+                true
+            } else {
+                false
+            }
+        }
     }
 
 
     fun setData(array: Array<Customers>): Array<Customers> {
         println("counterOfList $counterOfList")
         println("array.size ${array.size}")
-        if (counterOfList in (array.size-9)..array.size){
-            return array.sliceArray(counterOfList..(counterOfList + (array.size % 10) -1))
+        if (counterOfList in (array.size - (numberOfClassification - 1))..array.size) {
+            return array.sliceArray(counterOfList..(counterOfList + (array.size % numberOfClassification) - 1))
         } else {
-            return array.sliceArray(counterOfList..counterOfList + 9)
+            return array.sliceArray(counterOfList..counterOfList + (numberOfClassification - 1))
         }
     }
 
